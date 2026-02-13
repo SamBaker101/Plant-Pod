83313bb830d533c8d37551793c1624032135ac49
Sam Baker <Sam.Baker101@gmail.com>
2026-02-13 10:00:51 -0500

    Analog read for moisture sensor

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,12 @@
 #define LED_PIN 9
+#define ANALOG_PIN 7
 
 //ESP32 S3 Code for Plant Pod Project
 //Sam Baker - 2026
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(ANALOG_PIN, INPUT);
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -14,7 +16,9 @@ void setup() {
 void loop() {
   int read_data = get_read();
   if (read_data != -1) handle_read(read_data);
-  delay(50);
+  int analog_value = analogRead(ANALOG_PIN);
+  Serial.println(analog_value);
+  delay(100);
 }
 
 int get_read(){
